3ae788dfa5f2bad0f52a90612ea7c41f0cae2919
barronh <barronh@gmail.com>
2020-09-23 13:13:21 -0400

    Changing grid and output name

--- convertsat.sh
+++ convertsat.sh
@@ -2,7 +2,7 @@
 mkdir -p outsat
 python scripts/sat2cmaq.py \
   GRIDDESC \
-  HEMIS \
-  outsat/OMI-Aura_L2-OMNO2_2016m0101_v003.nc \
+  108NHEMI2 \
+  outsat/OMI-Aura_L3_108NHEMI2-OMNO2_2016m0101_v003.nc \
   config/OMNO2v003.py \
   /work/ROMO/users/bhenders/obs/OMNO2d/aura.gesdisc.eosdis.nasa.gov/data/Aura_OMI_Level2/OMNO2.003/2016/001/*
